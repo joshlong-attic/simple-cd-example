b0975b1c1955ed8bf8006d0efbf70873b07e8e82
Josh Long <josh@joshlong.com>
2016-05-09 12:47:40 +0200

    a simple microservice that we'll evolve, iteratively, for CNJ

---
+++ src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/hi")
+	Map<String, String> hello() {
+		return Collections.singletonMap("greeting", "Hello, world!");
+	}
+}
---
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -0,0 +1,42 @@
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DemoApplication.class)
+@WebAppConfiguration
+public class DemoApplicationTests {
+
+	@Autowired
+	private WebApplicationContext wac;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void before() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+	}
+
+	@Test
+	public void contextLoads() throws Exception {
+		this.mockMvc.perform(get("/hi"))
+				.andExpect(status().isOk())
+				.andExpect(mvcResult -> {
+					String contentAsString = mvcResult.getResponse().getContentAsString();
+					Assert.assertTrue(contentAsString.contains("Hello, world!"));
+				});
+	}
+}
